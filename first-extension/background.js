bb76c33bc4066ae8a12913f29c189af3e66fcc8f
danhchau-phan <chauphan253@gmail.com>
2021-02-12 21:27:54 +0800

    retrieved bookmarked urls

--- first-extension/background.js
+++ first-extension/background.js
@@ -1,5 +1,12 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    chrome.bookmarks.search(message.href, (result) => {
+        if (result.length > 0) {
+            sendResponse({href: message.href, bookmarked: true});
+        } else {
+            sendResponse({href: message.href, bookmarked: false});
+        };
+    });
+    
+    return true;
+})
 
-
-// chrome.runtime.onInstalled.addListener(() => {
-
-// });
--- first-extension/content/content.js
+++ first-extension/content/content.js
@@ -1,19 +1,18 @@
-function isBookmarked() {
-    
-    return true;
-}
-
-var gClasses = document.getElementsByClassName("g")
+var gClasses = document.getElementsByClassName("g");
 var i;
-for (i=0;i<gClasses.length; i++) {
+for (i=0;i<gClasses.length; i++) {    
     try {
-        var href_ = gClasses[i].firstElementChild
-            .firstElementChild
-            .getElementsByTagName("a")[0].href;
+        var href_ = gClasses[i].children[gClasses[i].children.length - 1]
+                    .firstElementChild
+                    .getElementsByTagName("a")[0].href;
 
-        console.log(href_);
-        if (isBookmarked(href_)) {
-            console.log(href_);
-        }
-    } catch (TypeError) {}
-}
+        chrome.runtime.sendMessage({href: href_}, (response) => {
+            if (response.bookmarked === true) {
+                console.log(response.href);
+            };
+        });
+        
+    } catch (TypeError) {
+        console.log(gClasses[i]);
+    }
+};
