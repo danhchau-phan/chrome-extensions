a9407d14c484e0e67a5095b148824fe23f2a2745
danhchau-phan <chauphan253@gmail.com>
2021-02-14 08:09:37 +0800

    create new element in DOM, web_accessible_resources fails to load

--- first-extension/background.js
+++ first-extension/background.js
@@ -1,12 +1,21 @@
+
+chrome.runtime.onMessage.addListener((message, callback) => {
+      if (message == "runContentScript"){
+        chrome.tabs.executeScript({
+          file: 'content/content.js'
+        });
+      }
+});
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     chrome.bookmarks.search(message.href, (result) => {
         if (result.length > 0) {
-            sendResponse({href: message.href, bookmarked: true});
+            sendResponse({href: message.href, index: message.index, bookmarked: true});
         } else {
             sendResponse({href: message.href, bookmarked: false});
         };
     });
     
     return true;
-})
+});
 
--- first-extension/content/content.js
+++ first-extension/content/content.js
@@ -1,3 +1,9 @@
+async function mark(element) {
+    var img = document.createElement("img");
+    // img.src = await chrome.runtime.getURL("images/icon.png");
+    element.getElementsByTagName("h3")[0].appendChild(img);
+}
+
 var gClasses = document.getElementsByClassName("g");
 var i;
 for (i=0;i<gClasses.length; i++) {    
@@ -6,9 +12,12 @@ for (i=0;i<gClasses.length; i++) {
                     .firstElementChild
                     .getElementsByTagName("a")[0].href;
 
-        chrome.runtime.sendMessage({href: href_}, (response) => {
+        chrome.runtime.sendMessage({href: href_, index: i}, (response) => {
             if (response.bookmarked === true) {
                 console.log(response.href);
+                mark(gClasses[response.index].children[gClasses[response.index].children.length - 1]
+                    .firstElementChild
+                    .getElementsByTagName("a")[0]);
             };
         });
         
